726a169aae8877263fc3b6ccbf73fbcb13269a5c
devang <devang.patil@upgrad.com>
2019-02-21 11:56:58 +0530

    React using Axios

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,6 @@
 import React from 'react'
 import { Link, NavLink, withRouter } from 'react-router-dom'
 const Navbar =(props) => {
-	setTimeout(() =>{
-		props.history.push('/about')
-	}, 2000)
 	return (
 		<nav className="nav-wrapper red darken-3">
 			<div className="container">
