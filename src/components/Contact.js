2a888ef5a2675181edcd59b12dfd58e707dff2a2
devang <devang.patil@upgrad.com>
2019-02-21 10:09:29 +0530

    Programmatic Redirects

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
-const Contact = () =>{
+const Contact = (props) =>{
+	setTimeout(() =>{
+		props.history.push('/about')
+	}, 2000)
 	return(
 		<div className="container">
 			<h4 className="center">Contact</h4>
 			<p>lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum </p>
+
 		</div>
 	)
 }
